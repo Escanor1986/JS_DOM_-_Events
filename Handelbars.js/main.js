05ea7ed70213038fc06de8e5d1ab60ed9957d894
Escanor1986 <lionel.zovi@gmail.com>
2024-03-12 14:24:34 +0100

    Chore_Door & Piano_keys projects finsihed

---
+++ Handelbars.js/main.js
@@ -0,0 +1,13 @@
+import Handlebars from "handlebars";
+const source = document.getElementById("ice-cream").innerHTML;
+
+const template = Handlebars.compile(source);
+
+const context = {
+  flavor: "chocolate",
+};
+
+const compiledHtml = template(context);
+
+const iceCreamText = document.getElementById("scream");
+iceCreamText.innerHTML = compiledHtml;
